77879eeccaf2e8a3d1d86224c99b63e10ad2b53e
Unknown <michaelfami@gmail.com>
2018-03-07 10:17:34 -0600

    moved back end

---
+++ routes/findPlayer.php
@@ -0,0 +1,18 @@
+<?php
+if (!empty($_GET['playerName'])) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.fortnitetracker.com/v1/profile/psn/" . $_GET['playerName']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'TRN-Api-Key: 47112942-ea0a-42d5-ba4e-17d64259a4e1'
+    ));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo $response;
+}
+
+
+
+?>
