bbfa6dc234661b8e499254102f4b88080a0edb5a
Unknown <brandonromo130@yahoo.com>
2018-03-07 13:09:15 -0600

    updated footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -44,18 +44,17 @@
         </div>
 
     <footer id="foot">
-        <a id="home" href="...">Home</a></br>
-        <a id="contact" href="contact.html">Contact Us</a></br>
-        <div id="copy" class="copyright">
-            <p>© 2018 Fortnite </p>
 
+        <div id="copy" class="copyright">
+            <h7>© 2018 Fortnite </h7>
+        </div>
     </footer>
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script src="app.js"></script>
+    <script src="js/fortnite.js"></script>
 
 </body>
 
